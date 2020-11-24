6ae691017baff32856f4f8755ad9cdc30404c53a
Ingo Fischer <mail@ingofischer.de>
2020-11-24 22:07:41 +0100

    Add sample tracks, implement reading tags from them

--- OpenTunes4/ContentView.swift
+++ OpenTunes4/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CoreData
+import AVFoundation
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -43,12 +44,51 @@ extension ContentView {
             super.init(dataSource: [Track]())
         }
         
+        func deleteAllTracks(context: NSManagedObjectContext) {
+            var fetchRequest = Track.fetchRequest() as NSFetchRequest<Track>
+            let coreDataTracks = try! context.fetch(fetchRequest) as [Track]
+            for track in coreDataTracks {
+                print("Deleting track")
+                context.delete(track)
+            }
+            try! context.save()
+        }
+        
         func loadTracks(context: NSManagedObjectContext) {
             self.context = context
+            
+            deleteAllTracks(context: context)
+            
+            let urls = Bundle.main.urls(forResourcesWithExtension: "mp3", subdirectory: nil)!
+            print("got tracks: \(urls.count)")
+            
+            for url in urls {
+                let asset = AVAsset(url: url)
+                let artist = getTagFilterByIdentifier(asset: asset, identifier: AVMetadataIdentifier.commonIdentifierArtist)
+                let title = getTagFilterByIdentifier(asset: asset, identifier: AVMetadataIdentifier.commonIdentifierTitle)
+                let url = url.relativeString
+                
+                let track = Track(context: context)
+                track.artist = artist
+                track.title = title
+                track.url = url
+                try! context.save()
+            }
+            
             let fetchRequest = Track.fetchRequest() as NSFetchRequest<Track>
-            let coreDataItems = try! context.fetch(fetchRequest) as [Track]
-            dataSource = coreDataItems
+            let coreDataTracks = try! context.fetch(fetchRequest) as [Track]
+            dataSource = coreDataTracks
         }
+    }
+}
+
+func getTagFilterByIdentifier(asset: AVAsset, identifier: AVMetadataIdentifier) -> String? {
+    let items = AVMetadataItem.metadataItems(from: asset.metadata, filteredByIdentifier: identifier)
+    
+    if items.count > 0 {
+        return items.first!.stringValue
+    } else {
+        return ""
     }
 }
 
